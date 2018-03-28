b776c70a439ed4fa232c3c9711373c0cc43122ca
ALEX BAKSH <s331394007@ocdsb>
2018-03-28 11:30:56 -0400

    main

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <Alex Baksh>
 # March 28, 2018
 
 ###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+    F=(1.8)*C+32
+    return F
+
 
 ###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
+def FtoC (F):
+    C = (0.55556)*(F-32)
+    return C
+    
+#temperature = int(input('Enter your temperature in Celsius: '))
+#print(CtoF(temperature))
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print('Enter 1 for Celsius to Fahrenheit, enter 2 for Fahrenheit to Celsius')
+choice = int(input())
+temperature = int(input())
+
+if choice == 1 and temperature <-273.15:
+    print('temperature must be above -273.15')
+else:
+    temperature = CtoF(temperature)
+    print(round(temperature))
+
+
+if choice == 1:
+    temperature = CtoF(temperature)
+    print(round(temperature))
+elif choice == 2:
+    temperature = FtoC(temperature)
+    print(round(temperature))
+else:
+    print('please enter 1 or 2')
+        
+    
